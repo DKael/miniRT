dc92419997dc2c445d62d17a4b2a1b3e2d3b1e24
DMKael <hyungdki@student.42seoul.kr>
2023-12-13 21:09:06 +0900

    .

--- minirt.h
+++ minirt.h
@@ -6,11 +6,46 @@
 /*   By: hyungdki <hyungdki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 16:04:54 by hyungdki          #+#    #+#             */
-/*   Updated: 2023/10/07 16:05:10 by hyungdki         ###   ########.fr       */
+/*   Updated: 2023/12/13 10:34:32 by hyungdki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <math.h>
+# include "../minilibx/mlx.h"
+# include "../minilibx/mlx_keycode.h"
+
+# if !defined(TRUE) && !defined(FALSE)
+#  define TRUE 1
+#  define FALSE 0
+# endif
+# ifndef T_NULL
+#  define T_NULL (void *)0
+# endif
+
+typedef int	t_bool;
+
+typedef struct s_vec
+{
+	double	x;
+	double	y;
+	double	z;
+}	t_vec;
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr[2];
+	char	*img_addr[2];
+}	t_data;
+
 
 # endif
