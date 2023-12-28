fb3e919764c674f9e2eb02425d37679bb80783a8
Gi Wan An <gan@cx2r3s1.42seoul.kr>
2023-12-28 15:01:54 +0900

    vec

---
+++ vec/vec_operation.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec_operation.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gan <gan@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 14:17:25 by gan               #+#    #+#             */
+/*   Updated: 2023/12/28 14:17:27 by gan              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vec.h"
+
+t_vec	vec_add(t_vec u, t_vec v) {
+	t_vec result;
+
+	result.x = u.x + v.x;
+	result.y = u.y + v.y;
+	result.z = u.z + v.z;
+	return (result);
+}
+
+t_vec	vec_sub(t_vec u, t_vec v) {
+	t_vec result;
+
+	result.x = u.x - v.x;
+	result.y = u.y - v.y;
+	result.z = u.z - v.z;
+	return (result);
+}
+
+t_vec	vec_multi(t_vec v, double d) {
+	t_vec	result;
+
+	result.x = v.x * d;
+	result.y = v.y * d;
+	result.z = v.z * d;
+	return (result);
+}
+
+double	vec_length(t_vec v) {
+	return (sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2)));
+}
